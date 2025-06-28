f84b88437495445801b56072d5132cb525c753f3
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-06-28 09:51:11 +0200

    Initial Commit

---
+++ ClimaSwiftUI/ClimaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaSwiftUIApp.swift
+//  ClimaSwiftUI
+//
+//  Created by Elfo on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClimaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
